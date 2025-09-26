3d132c6de791efba4dd6b07b444e35085b0954f4
Kauã Ribeiro <kauaribeirodev@outlook.com>
2025-09-26 15:43:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
+  // ADICIONE ESTA LINHA:
+  base: "/portfolio-victoroliveira/", 
+
   plugins: [react(), cloudflare()],
   server: {
     allowedHosts: true,
