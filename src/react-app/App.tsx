62e4059ea7dd41c3550392add247e8b6a9d79319
Kauã Ribeiro <kauaribeirodev@outlook.com>
2025-09-26 15:44:26 -0300

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,8 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+// ALTERE A IMPORTAÇÃO DE "BrowserRouter" PARA "HashRouter"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Use react-router-dom
 import HomePage from "@/react-app/pages/Home";
 
 export default function App() {
   return (
+    // ALTERE A TAG DE "BrowserRouter" PARA "Router" (que agora é HashRouter)
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
