bfe3857f4879f45939efb7c34e2dd517ac2a692a
Kauã Ribeiro <kauaribeirodev@outlook.com>
2025-09-26 15:59:21 -0300

    fix: Instala react-router-dom e remove react-router

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,10 +1,8 @@
-// ALTERE A IMPORTAÇÃO DE "BrowserRouter" PARA "HashRouter"
-import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Use react-router-dom
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "@/react-app/pages/Home";
 
 export default function App() {
   return (
-    // ALTERE A TAG DE "BrowserRouter" PARA "Router" (que agora é HashRouter)
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
